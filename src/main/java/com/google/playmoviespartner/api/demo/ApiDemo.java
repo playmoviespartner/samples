c5659cba798f1aac5c6a4fe587f474a79ab70a4c
Felipe Leme <felipeal@google.com>
2015-08-25 11:29:37 -0700

    Use Builder to set application name and avoid warning on constructor.

--- src/main/java/com/google/playmoviespartner/api/demo/ApiDemo.java
+++ src/main/java/com/google/playmoviespartner/api/demo/ApiDemo.java
@@ -1,70 +1,19 @@
 package com.google.playmoviespartner.api.demo;
 
 import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
-import com.google.api.client.http.GenericUrl;
-import com.google.api.client.http.HttpHeaders;
-import com.google.api.client.http.HttpRequest;
-import com.google.api.client.http.HttpRequestFactory;
 import com.google.api.client.http.HttpRequestInitializer;
-import com.google.api.client.http.HttpResponse;
 import com.google.api.client.http.HttpTransport;
-import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.gson.Gson;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.services.playmoviespartner.Playmoviespartner;
+import com.google.api.services.playmoviespartner.PlaymoviespartnerScopes;
+import com.google.api.services.playmoviespartner.model.ListOrdersResponse;
+import com.google.api.services.playmoviespartner.model.Order;
 
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
 import java.util.Collections;
 import java.util.List;
 
 public final class ApiDemo {
-
-  private static final String API_SCOPE = "https://www.googleapis.com/auth/playmovies_partner.readonly";
-
-  private static final String BASE_API = "https://playmoviespartner.googleapis.com";
-
-  private static String listUnfulfilledOrdersUrl(String accountId, int size,
-      String nextPageToken) {
-    String url = String.format(
-        "%s/v1/accounts/%s/orders?status=STATUS_UNFULFILLED&pageSize=%d",
-        BASE_API, accountId, size);
-    if (nextPageToken != null) {
-      try {
-        url += "&pageToken="
-            + URLEncoder.encode(nextPageToken,
-                StandardCharsets.UTF_8.toString());
-      } catch (UnsupportedEncodingException e) {
-        throw new RuntimeException("Error encoding " + nextPageToken, e);
-      }
-    }
-    return url;
-  }
-
-  private static ListOrdersResponse listUnfulFilledOrders(
-      GoogleCredential credential, String accountId, int batchSize,
-      String nextPageToken) throws IOException {
-    credential.refreshToken();
-    final String accessToken = credential.getAccessToken();
-
-    HttpTransport transport = new NetHttpTransport();
-    HttpRequestFactory httpBasicFactory = transport
-        .createRequestFactory(new HttpRequestInitializer() {
-          @Override
-          public void initialize(HttpRequest request) {
-            HttpHeaders headers = new HttpHeaders();
-            headers.setAuthorization("Bearer " + accessToken);
-            request.setHeaders(headers);
-          }
-        });
-    String url = listUnfulfilledOrdersUrl(accountId, batchSize, nextPageToken);
-//  System.out.println("URL: " + url);
-    HttpRequest request = httpBasicFactory.buildGetRequest(new GenericUrl(url));
-    HttpResponse response = request.execute();
-    String responseJson = response.parseAsString();
-    Gson gson = new Gson();
-    return gson.fromJson(responseJson, ListOrdersResponse.class);
-  }
 
   public static void main(final String[] args) throws Exception {
     if (args.length != 3) {
@@ -75,8 +24,18 @@ public final class ApiDemo {
     int batchSize = Integer.parseInt(args[1]);
     int maxCalls = Integer.parseInt(args[2]);
 
-    final GoogleCredential credential = GoogleCredential.getApplicationDefault()
-        .createScoped(Collections.singletonList(API_SCOPE));
+    GoogleCredential credential = GoogleCredential.getApplicationDefault()
+        .createScoped(Collections.singletonList(
+            PlaymoviespartnerScopes.PLAYMOVIES_PARTNER_READONLY));
+    credential.refreshToken();
+    HttpTransport transport = credential.getTransport();
+    JsonFactory jsonFactory = credential.getJsonFactory();
+    HttpRequestInitializer httpRequestInitializer = credential;
+
+    Playmoviespartner pmp =
+        new Playmoviespartner.Builder(transport, jsonFactory, httpRequestInitializer)
+            .setApplicationName("Demo application")
+            .build();
 
     String nextPageToken = null;
     int callNumber = 0;
@@ -88,9 +47,12 @@ public final class ApiDemo {
       callNumber++;
       lastTry = callNumber == maxCalls;
       try {
-        ListOrdersResponse ordersResponse =
-            listUnfulFilledOrders(credential, accountId, batchSize, nextPageToken);
-        // System.out.println("Response: " + ordersResponse);
+
+        ListOrdersResponse ordersResponse = pmp.accounts().orders().list(accountId)
+            .setPageSize(batchSize)
+            .setPageToken(nextPageToken)
+            .execute();
+
         List<Order> orders = ordersResponse.getOrders();
         nextPageToken = ordersResponse.getNextPageToken();
         System.out.printf("\nCall #%d returned %d unfulfilled orders:\n\n", callNumber,
